0554833b8f56bbcddfaa43a51c104254a53e31f0
vgzowski <vovmorozov2@gmail.com>
2024-06-01 16:50:14 +0200

    Add .env config for db

--- src/main/java/MMM/demo/SlavicGymApplication.java
+++ src/main/java/MMM/demo/SlavicGymApplication.java
@@ -3,7 +3,10 @@ package MMM.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.PropertySource;
+
 @SpringBootApplication
+@PropertySource("classpath:.env")
 public class SlavicGymApplication {
 
 	public static void main(String[] args) {

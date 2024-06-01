82004696c94ae3e307d4e801c09b659ef47a6457
vgzowski <vovmorozov2@gmail.com>
2024-06-01 16:46:43 +0200

    Initialized spring-boot

---
+++ src/main/java/MMM/demo/SlavicGymApplication.java
@@ -0,0 +1,13 @@
+package MMM.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlavicGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlavicGymApplication.class, args);
+	}
+
+}

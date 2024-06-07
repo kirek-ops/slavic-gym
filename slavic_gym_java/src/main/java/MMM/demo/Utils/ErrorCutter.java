ee3c271e65aa3708f4044d3f6bfe99e8f21faf07
vgzowski <vovmorozov2@gmail.com>
2024-06-07 19:42:51 +0200

    Update error trimmer

--- slavic_gym_java/src/main/java/MMM/demo/Utils/ErrorCutter.java
+++ slavic_gym_java/src/main/java/MMM/demo/Utils/ErrorCutter.java
@@ -1,12 +1,11 @@
 package MMM.demo.Utils;
 
 public class ErrorCutter {
-  public static String cut(String errorMessage) {
+  public static String cut(String errorMessageOrig) {
+    String errorMessage = new String(errorMessageOrig);
     int errorIndex = errorMessage.indexOf("ERROR");
     if (errorIndex != -1) {
       errorMessage = errorMessage.substring(errorIndex);
-    } else {
-      return errorMessage;
     }
     int whereIndex = errorMessage.indexOf("Where:");
     if (whereIndex != -1) {
